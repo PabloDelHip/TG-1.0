29b63596a59fcde7c82ac0b83bb723f6d660bdeb
figocancun <figocancun@hotmail.es>
2018-02-02 16:12:26 -0500

    Cambios

--- CapaPresentacion/FrmReporteMovCaja.cs
+++ CapaPresentacion/FrmReporteMovCaja.cs
@@ -12,6 +12,8 @@ namespace CapaPresentacion
 {
     public partial class FrmReporteMovCaja : Form
     {
+        public int idUsuario;
+        public DateTime fecha;
         public int opc;
         public FrmReporteMovCaja()
         {
@@ -20,9 +22,18 @@ namespace CapaPresentacion
 
         private void FrmReporteMovCaja_Load(object sender, EventArgs e)
         {
-            FrmCortesCaja cortesCaja = new FrmCortesCaja();
-            opc = cortesCaja.opcion;
-            MessageBox.Show(opc.ToString());
+            CRMovCaja reporteCaja = new CRMovCaja();
+            CRMovCajaFecha reporteCajaFecha = new CRMovCajaFecha();
+            if (opc==1)
+            {
+                reporteCajaFecha.SetParameterValue("@FechaHora", fecha);
+                crvMovCaja.ReportSource = reporteCajaFecha;
+            }
+            else if (opc==2)
+            {
+                reporteCaja.SetParameterValue("@idUsuario", idUsuario);
+                crvMovCaja.ReportSource = reporteCaja;
+            }
         }
     }
 }
