042c42457f958b1627a013e08b61c23ecab17b47
Pablo Delfin <pablodelhip@gmail.com>
2018-02-16 13:22:14 -0600

    cambios

--- CapaPresentacion/FrmLogin.Designer.cs
+++ CapaPresentacion/FrmLogin.Designer.cs
@@ -75,7 +75,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(271, 69);
+            this.button1.Location = new System.Drawing.Point(271, 89);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
